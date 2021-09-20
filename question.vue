f48ee900e8680d53a018d4bdcffcc1be921a3552
Borislav Lepsanovic <57566134+Borizle@users.noreply.github.com>
2021-09-20 19:14:01 +0200

    Update question.vue

--- question.vue
+++ question.vue
@@ -1,4 +1,5 @@
 <template>
+<!-- 1. Missing the main <div>. -->
   <div class="loader-wrapper" v-show="loading">
     <p>Loading...</p>
   </div>
@@ -8,6 +9,9 @@
   <div class="list-wrapper" v-show="listItem.length">
     <h1 class="list-wrapper__title">List Items</h1>
     <ul class="list-wrapper__list">
+      <!-- 2.1 the component is not well defined.  -->
+      <!-- 2.2 not mix v-for and v-if  -->
+      <!-- 2.3 var listItems should be displayed by some filtering method -->
       <list-item
         v-for="item in listItems"
         v-if="item.active"
@@ -19,11 +23,13 @@
 </template>
 
 <script>
+// 3. missing import components
 export default {
   components: {
     ListItem
   },
   data() {
+  // 4. Missing return
     listItems: [],
     showErrorMessage: false,
     loading: true
